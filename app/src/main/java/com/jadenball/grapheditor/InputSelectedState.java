d58e224480cd80759ce396f5bebf88319e60b95a
jjball <jadenjball@gmail.com>
2017-02-11 18:57:18 -0600

    Added more comments/documentation to the code

--- app/src/main/java/com/jadenball/grapheditor/InputSelectedState.java
+++ app/src/main/java/com/jadenball/grapheditor/InputSelectedState.java
@@ -7,7 +7,10 @@ import android.view.MotionEvent;
 public class InputSelectedState implements InputState {
 
 
-
+    /**
+     * class gListener
+     * Detects whether or not a long press happened
+     */
     class gListener extends GestureDetector.SimpleOnGestureListener {
         public void onLongPress(MotionEvent event){
             super.onLongPress(event);
@@ -18,13 +21,17 @@ public class InputSelectedState implements InputState {
     @Override
     public void handleTouch(GraphViewController c, MotionEvent event) {
 
+        // checking the gesture detector to see if a long press happened
         GestureDetector gDetector = new GestureDetector(c.model.view.getContext(), new gListener());
+        // TODO: FIX BUG - The result is always true. The gesture detector seems to think it is always longPressed when pressed.
         boolean result = gDetector.isLongpressEnabled();
 
-
+        // if it is not a long press, the user is moving their finger
+        // otherwise, create the edge and move to the long press state
         if(!result) {
             switch (event.getAction()) {
                 case MotionEvent.ACTION_UP:
+                    // change the state back to the ready state
                     c.selected = null;
                     c.notifySubscribers();
                     c.state = new InputReadyState();
@@ -38,6 +45,7 @@ public class InputSelectedState implements InputState {
             }
         }
         else{
+            // changing the state to the long pressed state
             c.model.createEdge(event.getX(), event.getX(), event.getY(), event.getY());
             c.state = new InputLongPressState();
             c.longSelected = c.selected;
