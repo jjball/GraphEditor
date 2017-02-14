5f4bd7d6468061e6a3b52dbc363c73a15c1b4e06
jjball <jadenjball@gmail.com>
2017-02-14 17:44:23 -0600

    Added drawing of the vertices and edges on the view

--- app/src/main/java/com/jadenball/grapheditor/MiniGraphView.java
+++ app/src/main/java/com/jadenball/grapheditor/MiniGraphView.java
@@ -2,6 +2,7 @@ package com.jadenball.grapheditor;
 
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
@@ -9,11 +10,93 @@ import android.view.View;
 public class MiniGraphView extends View {
 
     Paint myPaint;
+    GraphModel model;
+    GraphViewController controller;
+    GraphView parentView;
+    private float viewWidth;
+    private float viewHeight;
+
+    /**
+     * the scale of the mini view in relation to the full size view
+     */
+    private float scale = 0;
 
     public MiniGraphView(Context context) {
         super(context);
         myPaint = new Paint();
         myPaint.setColor(Color.BLUE);
         this.setBackgroundColor(Color.LTGRAY);
+        setViewSize(400, 400);
+    }
+
+    public void modelChanged(){
+        invalidate();
+    }
+
+    public void setModel(GraphModel g){
+        model = g;
+    }
+
+    public void setController(GraphViewController g){
+        controller = g;
+    }
+
+    /**
+     * Sets the view that this view will replicate a mini version of
+     * @param g the graph view we want a mini view of
+     */
+    public void setParentView(GraphView g){
+        parentView = g;
+        setScale();
+    }
+
+    public void onDraw(Canvas canvas){
+        myPaint.setColor(Color.GRAY);
+        canvas.drawRect(parentView.getViewPosX() * scale, parentView.getViewPosY() * scale,
+                parentView.getWidth() * scale, parentView.getHeight() * scale, myPaint);
+
+        for(Edge e : model.edges){
+            myPaint.setColor(Color.BLACK);
+            canvas.drawLine(e.getX1() * scale, e.getY1() * scale, e.getX2() * scale, e.getY2()* scale, myPaint);
+        }
+
+        for(Vertex v : model.vertices){
+            myPaint.setColor(Color.BLUE);
+            canvas.drawCircle(v.x * scale, v.y * scale, v.getRadius() * scale, myPaint);
+
+        }
+    }
+
+    public float getViewWidth() {
+        return viewWidth;
+    }
+
+    public void setViewWidth(float viewWidth) {
+        this.viewWidth = viewWidth;
+    }
+
+    public float getViewHeight() {
+        return viewHeight;
+    }
+
+    public void setViewHeight(float viewHeight) {
+        this.viewHeight = viewHeight;
+    }
+
+    public void setViewSize(float width, float height){
+        setViewWidth(width);
+        setViewHeight(height);
+    }
+
+    public void setScale(float newScale){
+        scale = newScale;
+    }
+
+    private void setScale(){
+        scale = viewWidth / parentView.getViewWidth();
+    }
+
+    public float getScale(){
+        return scale;
     }
 }
